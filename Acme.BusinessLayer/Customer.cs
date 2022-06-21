f5131213c2736b55ccbbccb458825e29af7fde71
al-fian <st118436@alumni.ait.ac.th>
2022-06-21 21:57:49 +0700

    Added NUnit test for Customer

--- Acme.BusinessLayer/Customer.cs
+++ Acme.BusinessLayer/Customer.cs
@@ -5,7 +5,19 @@
         public int CustomerId { get; private set; }
         public string? FirstName { get; set; } 
         public string? LastName { get; set; }
-        public string? FullName => $"{LastName}, {FirstName}";
+
+        public string FullName 
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(FirstName))
+                    throw new ArgumentNullException("FirstName cannot be empty");
+                if (string.IsNullOrWhiteSpace(LastName))
+                    throw new ArgumentNullException("LastName cannot be empty");
+
+                return $"{LastName}, {FirstName}";
+            }
+        }
         public string? EmailAddress { get; set; }
 
     }
--- NUnitTest/Acme.BusinessLayer.NUnitTests/CustomerTests.cs
+++ NUnitTest/Acme.BusinessLayer.NUnitTests/CustomerTests.cs
@@ -16,5 +16,67 @@ namespace Acme.BusinessLayer.NUnitTests
 
             Assert.That(expected, Is.EqualTo(actual));
         }
+
+        [Test]
+        public void FullName_GivenFirstNameNull_ThrowsArgumentNullException()
+        {
+            var customer = new Customer();
+            customer.FirstName = null;
+            customer.LastName = "Baggins";
+
+            Assert.That(() => customer.FullName, Throws.Exception.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void FullName_GivenLastNameNull_ThrowsArgumentNullException()
+        {
+            var customer = new Customer();
+            customer.FirstName = "Frodo";
+            customer.LastName = null;
+
+            Assert.That(() => customer.FullName, Throws.Exception.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void FullName_GivenFirstNameEmptyString_ThrowsArgumentNullException()
+        {
+            var customer = new Customer();
+            customer.FirstName = "";
+            customer.LastName = "Baggins";
+
+            Assert.That(() => customer.FullName, Throws.Exception.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void FullName_GivenLastNameEmptyString_ThrowsArgumentNullException()
+        {
+            var customer = new Customer();
+            customer.FirstName = "Frodo";
+            customer.LastName = "";
+
+            Assert.That(() => customer.FullName, Throws.Exception.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void FullName_GivenFirstNameEmptySpace_ThrowsArgumentNullException()
+        {
+            var customer = new Customer();
+            customer.FirstName = " ";
+            customer.LastName = "Baggins";
+
+            Assert.That(() => customer.FullName, Throws.Exception.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void FullName_GivenLastNameEmptySpace_ThrowsArgumentNullException()
+        {
+            var customer = new Customer();
+            customer.FirstName = "Frodo";
+            customer.LastName = " ";
+
+            Assert.That(() => customer.FullName, Throws.Exception.TypeOf<ArgumentNullException>());
+        }
+
+
     }
 }
