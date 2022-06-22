3265546eda43d5f14c5cee2443007ad619149d33
al-fian <st118436@alumni.ait.ac.th>
2022-06-22 08:44:03 +0700

    XUnit.net testing FirstName and LastName properties

--- XUnitTest/Acme.BusinessLayer.XUnitTests/CustomerTest.cs
+++ XUnitTest/Acme.BusinessLayer.XUnitTests/CustomerTest.cs
@@ -2,6 +2,55 @@ namespace Acme.BusinessLayer.XUnitTests
 {
     public class CustomerTest
     {
+
+        [Fact]
+        public void FirstName_GivenFirstNameNull_ThrowsArgumentNullException()
+        {
+            var customer = new Customer();
+
+            Assert.Throws<ArgumentNullException>(() => customer.FirstName = null);  
+        }
+
+        [Fact]
+        public void FirstName_GivenFirstNameEmptyString_ThrowsArgumentNullException()
+        {
+            var customer = new Customer();
+
+            Assert.Throws<ArgumentNullException>(() => customer.FirstName = "");
+        }
+
+        [Fact]
+        public void FirstName_GivenFirstNameEmptySpace_ThrowsArgumentNullException()
+        {
+            var customer = new Customer();
+
+            Assert.Throws<ArgumentNullException>(() => customer.FirstName = " ");
+        }
+
+        [Fact]
+        public void LastName_GivenLastNameNull_ThrowsArgumentNullException()
+        {
+            var customer = new Customer();
+
+            Assert.Throws<ArgumentNullException>(() => customer.LastName = null);
+        }
+
+        [Fact]
+        public void LastName_GivenLastNameEmptyString_ThrowsArgumentNullException()
+        {
+            var customer = new Customer();
+
+            Assert.Throws<ArgumentNullException>(() => customer.LastName = "");
+        }
+
+        [Fact]
+        public void LastName_GivenLastNameEmptySpace_ThrowsArgumentNullException()
+        {
+            var customer = new Customer();
+
+            Assert.Throws<ArgumentNullException>(() => customer.LastName = " ");
+        }
+
         [Fact]
         public void FullName_GivenFirstNameAndLastName_ReturnFullName()
         {
